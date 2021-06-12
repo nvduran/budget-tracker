31a21848e2d148ee65db4d80fcff4326d5048949
nvduran <nvduran@gmail.com>
2021-06-12 13:16:27 -0500

    Deploying

--- server.js
+++ server.js
@@ -9,19 +9,22 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+//   useFindAndModify: false,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
